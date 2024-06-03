505f755819fe3306456566dd1f4d9a0a83b7717d
olaniyanp11 <107733620+olaniyanp11@users.noreply.github.com>
2024-06-03 13:55:30 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,8 @@ export const Footer = () => {
     return (
         <div className='text-white w-full text-center my-10 pt-10 border-t border-neutral-600 '>
            &copy; All rights reserved by permacTech
+            <span className="w-full"> 09039449932 </span>
+            <span>olaniyanp11@gmail.com</span>
         </div>
     )
 }
